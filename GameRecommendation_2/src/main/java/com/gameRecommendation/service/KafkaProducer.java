fbf8c13a65cd2300e347252673804cad1f287745
khushbooshaw779 <khushbooshaw779@gmail.com>
2018-05-14 08:49:22 +0530

    addedtherecommendation service without POST method (which is not working)

---
+++ GameRecommendation_2/src/main/java/com/gameRecommendation/service/KafkaProducer.java
@@ -0,0 +1,43 @@
+//package com.gameRecommendation.service;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//import com.gameRecommendation.domain.Topics;
+//import com.gameRecommendation.domain.User;
+//
+//
+//
+//@Service
+//public class KafkaProducer {
+
+//	private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+//
+//	@Autowired
+//	private KafkaTemplate<String, Object> kafkaTemplate;
+//
+//	@Value("${jsa.kafka.topic}")
+//	String kafkaTopic;
+//
+//	public void sendUser(User user) {
+//
+//		log.info("sending data='{}'", user);
+//		kafkaTemplate.send("recommendation-user", user);
+//	}
+//	public void sendGame(Topics game) {
+//
+//		log.info("sending data='{}'", game);
+//		kafkaTemplate.send("recommendation-game", game);
+//	}
+//	public void sendRecommended(String[] game) {
+//
+//		log.info("sending data='{}'", game.length);
+//		kafkaTemplate.send("recommendation-game-by-user-friends", game);
+//	}
+//	
+//}
+
